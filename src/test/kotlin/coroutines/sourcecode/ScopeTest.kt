0c61f9feedb117d4dfc9d0875e0629b0e4650fa7
andannn <jqn296763005@gmail.com>
2024-10-29 17:37:33 +0900

    test

--- src/test/kotlin/coroutines/sourcecode/ScopeTest.kt
+++ src/test/kotlin/coroutines/sourcecode/ScopeTest.kt
@@ -5,11 +5,13 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.Test
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
+import kotlin.coroutines.suspendCoroutine
 import kotlin.test.assertEquals
 
 class ScopeTest {
@@ -41,5 +43,35 @@ class ScopeTest {
         scope.cancel()
         scope.coroutineContext[Job]!!.join()
         assertEquals(2, canceledCount)
+    }
+
+    @Test
+    fun scope_cancel_test2() = testScope.runTest {
+
+
+        val scope = CoroutineScope(Dispatchers.Default)
+
+        var canceledCount = 0
+        scope.launch {
+            try {
+                launch_sub_coroutine()
+            } catch (e: CancellationException) {
+                canceledCount++
+            }
+        }
+
+        delay(1000)
+        scope.coroutineContext.cancel()
+//        scope.cancel()
+        scope.coroutineContext[Job]!!.join()
+        assertEquals(1, canceledCount)
+    }
+
+    private suspend fun launch_sub_coroutine() = coroutineScope {
+        launch {
+            while (true) {
+                delay(100)
+            }
+        }
     }
 }
